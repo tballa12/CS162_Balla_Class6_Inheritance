b1217db48ff8b043cb1038ddc4550ace1a9c55fe
Terry Balla <tbsunder@outlook.com>
2020-04-16 08:24:39 -0700

    Added Todo  Employee

---
+++ src/Employee.java
@@ -0,0 +1,46 @@
+/**
+ * Employee.java - Employee Benefits Management
+ * @author Kimberly Bryant
+ * @version 1
+ */
+public class Employee {
+    private String name;
+    private double baseSalary;
+    private int yearsOfExperience;
+    private int yearsAtCompany;
+
+    /**
+     * Parameterless constructor
+     */
+    public Employee(){
+        //TODO use default values
+    }
+
+    /**
+     * Parameterless constructor
+     *@param TODO A variable of type TODO
+     *@param TODO A variable of type TODO
+     *@param TODO A variable of type TODO
+     *@param TODO A variable of type TODO
+     */
+    public Employee(/*TODO*/){
+        //TODO
+    }
+
+    //TODO Add needed getters and setters
+
+    /**
+     * Returns the employees name, salary and motto
+     * @return A value of data type String
+     */
+    public String toString(){
+        //TODO
+    }
+    /**
+     * Returns a generic employee motto
+     * @return A value of data type String
+     */
+    public String motto(){
+        return "We value our employees";
+    }
+}
